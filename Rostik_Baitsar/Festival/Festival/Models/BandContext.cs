b64d236887965228d31961b8c16ae573dd9da9b7
Rostik <rostik1804@gmail.com>
2019-07-24 01:46:47 +0300

    MKS-1 Add model

---
+++ Rostik_Baitsar/Festival/Festival/Models/BandContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Festival.Models
+{
+    public class BandContext : DbContext
+    {
+        public DbSet<Band> Bands { get; set; }
+
+        public BandContext(DbContextOptions<BandContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
