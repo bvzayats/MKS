b769faa332dea3caf910b8500b54e632486f4936
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-24 20:36:25 +0300

    MKS-46 Add UsersRepository

---
+++ Serhii Nykon/NedoNet.API/Repositories/IUsersRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+using NedoNet.API.Data.Models;
+using NedoNet.API.Entities;
+
+namespace NedoNet.API.Repositories {
+    public interface IUsersRepository {
+        Task<UserViewEntity> GetById( Guid userId );
+    }
+}
