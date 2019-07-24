cb47599104f11f36e9e3e77baf2e4fe22c323884
Volodymyr Tsisinskyj <tsisinskyjv@gmail.com>
2019-07-24 21:53:40 +0300

    MKS-36 remove unused namespaces

--- Volodymyr_Tsisinskyi/Nedo_net/Nedo_net/Entities/Student.cs
+++ Volodymyr_Tsisinskyi/Nedo_net/Nedo_net/Entities/Student.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Nedo_net.Entities
+﻿namespace Nedo_net.Entities
 {
     public class Student
     {
