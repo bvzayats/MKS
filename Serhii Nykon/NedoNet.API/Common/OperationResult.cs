59935c63b923074d1ace40ac177999e9b9249fe2
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-24 23:10:48 +0300

    MKS-46 Add OperationResult class

---
+++ Serhii Nykon/NedoNet.API/Common/OperationResult.cs
@@ -0,0 +1,21 @@
+﻿namespace NedoNet.API.Common {
+    public class OperationResult {
+        private OperationResult( bool isSuccess, string errorMessage, object result ) {
+            IsSuccess = isSuccess;
+            ErrorMessage = errorMessage;
+            Result = result;
+        }
+
+        public bool IsSuccess { get; set; }
+        public string ErrorMessage { get; set; }
+        public object Result { get; set; }
+
+        public static OperationResult Success(object result = null) {
+            return new OperationResult(isSuccess: true, errorMessage: null, result: result);
+        }
+
+        public static OperationResult Error( string errorMessage, object result = null ) {
+            return new OperationResult(isSuccess: false, errorMessage: errorMessage, result: result);
+        }
+    }
+}
