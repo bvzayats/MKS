3c49529feebc34876f79e21e93c3f741cecaad12
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-25 00:58:45 +0300

    MKS-46 Add Password property to the model and remove Attributes

--- Serhii Nykon/NedoNet.API/Data/Models/User.cs
+++ Serhii Nykon/NedoNet.API/Data/Models/User.cs
@@ -4,11 +4,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NedoNet.API.Data.Models {
     public class User {
-
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
