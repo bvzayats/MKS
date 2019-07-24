fdfee00e76ab14ee159f27f350326494433bc93f
Volodymyr Tsisinskyj <tsisinskyjv@gmail.com>
2019-07-24 16:04:24 +0300

    MKS-36 add entity in project

---
+++ Volodymyr_Tsisinskyi/Nedo_net/Nedo_net/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nedo_net.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public bool IsGranted { get; set; }
+        public string Email { get; set; }
+    }
+}
