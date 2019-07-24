bb15c19cc2fb6e667ac7a84d99a4c5ed84ed9648
Bohdan_khamurda <bogdan.hamurda@gmail.com>
2019-07-24 19:07:22 +0300

    MKS-31 Add class entity and class context

---
+++ Bohdan_Khamurda/WebAPISwagger/WebAPISwagger/Models/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPISwagger
+{
+    public partial class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Faculty { get; set; }
+        public int Course { get; set; }
+        public int? NumberHostel { get; set; }
+    }
+}
---
+++ Bohdan_Khamurda/WebAPISwagger/WebAPISwagger/UniversityContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAPISwagger
+{
+    public partial class UniversityContext : DbContext
+    {
+        public UniversityContext()
+        {
+        }
+
+        public UniversityContext(DbContextOptions<UniversityContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=University;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Students>(entity =>
+            {
+
+
+                entity.Property(e => e.Faculty).IsRequired();
+
+                entity.Property(e => e.Name).IsRequired();
+
+                entity.Property(e => e.Surname).IsRequired();
+            });
+        }
+    }
+}
