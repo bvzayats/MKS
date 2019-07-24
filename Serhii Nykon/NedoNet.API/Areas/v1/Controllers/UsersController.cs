c0d31de1dc089ad9d1da27d4ac7e835bee86e288
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-24 23:16:44 +0300

    MKS-46 Add UsersController with HttpGet operations

---
+++ Serhii Nykon/NedoNet.API/Areas/v1/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NedoNet.API.Services;
+
+namespace NedoNet.API.Areas.v1.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase {
+        private readonly IUsersService _usersService;
+
+        public UsersController( IUsersService usersService ) {
+            _usersService = usersService;
+        }
+
+        [HttpGet]
+        [Route("user")]
+        public async Task<IActionResult> GetUserAsync([FromQuery] Guid id) {
+            var result = await _usersService.GetUserAsync( id );
+
+            return Ok(result.Result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsersPage([FromQuery] int page) {
+            var result = await _usersService.GetPageAsync( page );
+            
+            return Ok(result.Result);
+        }
+    }
+}
