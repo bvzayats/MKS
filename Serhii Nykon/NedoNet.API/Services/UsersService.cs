d1aa21fd8e16b8f5b78366a7eb50e383bfc38ad4
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-24 20:38:16 +0300

    MKS-46 Add UserServices

---
+++ Serhii Nykon/NedoNet.API/Services/UsersService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using NedoNet.API.Entities;
+using NedoNet.API.UOF;
+
+namespace NedoNet.API.Services {
+    public class UsersService : IUsersService {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UsersService( IUnitOfWork unitOfWork ) {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<UserViewEntity> GetUser( Guid id ) {
+            return await _unitOfWork.UserRepository.GetById( id );
+        }
+    }
+}
