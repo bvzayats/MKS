6221bf8b77b88ce2424bd1c72f870f7035f65914
Rostik <rostik1804@gmail.com>
2019-07-24 16:31:36 +0300

    MKS-21 Remove unusing namespaces

--- Rostik_Baitsar/Festival/Festival/Startup.cs
+++ Rostik_Baitsar/Festival/Festival/Startup.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Festival.Models;
-using Festival.Options;
+﻿using Festival.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace Festival
@@ -53,7 +45,7 @@ namespace Festival
             }
 
             var swaggerOptions = new Options.SwaggerOptions();
-            Configuration.GetSection(nameof(Options.SwaggerOptions)).Bind(swaggerOptions);
+            Configuration.GetSection(nameof(Swashbuckle.AspNetCore.Swagger.SwaggerOptions)).Bind(swaggerOptions);
 
             app.UseSwagger(option =>
             {
