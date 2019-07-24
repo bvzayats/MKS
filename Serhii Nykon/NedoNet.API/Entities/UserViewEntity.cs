bf781e3665a1062d7494721399dc8e6f04266ef2
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-24 22:27:39 +0300

    Revert "MKS-46 Create UserViewEntity"
    This reverts commit 6f906d9cbab7a07dd2b85e72580ba61940aaf0fc.

--- Serhii Nykon/NedoNet.API/Entities/UserViewEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NedoNet.API.Entities
-{
-    public class UserViewEntity
-    {
-        public string Email { get; set; }
-        public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
-    }
-}
