0e3b404356c7117e4f99ea363bff2514115361d2
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-24 20:36:54 +0300

    MKS-46 Add MappingProfile

---
+++ Serhii Nykon/NedoNet.API/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using NedoNet.API.Data.Models;
+using NedoNet.API.Entities;
+
+namespace NedoNet.API {
+    public class MappingProfile : Profile {
+        public MappingProfile() {
+            #region Entities -> Models
+
+            CreateMap<User, UserViewEntity>()
+                .ForMember(
+                    dest => dest.FullName,
+                    opts => opts.MapFrom(src => src.FirstName + " " + src.LastName));
+
+            #endregion
+            
+            #region Models -> Entities
+
+            
+
+            #endregion
+        }
+    }
+}
