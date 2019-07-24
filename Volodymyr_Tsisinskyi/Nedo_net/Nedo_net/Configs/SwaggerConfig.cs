34fb394d30b74a1e07500bb1ab0a67102ec891f3
Volodymyr Tsisinskyj <tsisinskyjv@gmail.com>
2019-07-24 21:55:55 +0300

    MKS-42 remove unused namespaces

--- Volodymyr_Tsisinskyi/Nedo_net/Nedo_net/Configs/SwaggerConfig.cs
+++ Volodymyr_Tsisinskyi/Nedo_net/Nedo_net/Configs/SwaggerConfig.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Nedo_net.Configs
+﻿namespace Nedo_net.Configs
 {
     public class SwaggerConfig
     {
