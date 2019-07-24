10226fab7b43600f09b59bb3c7acd5df498cab13
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-24 22:27:25 +0300

    Revert "MKS-46 Add UnitOfWork"
    This reverts commit 094210f400fd70d15dde6544c7cc1c93666cbad2.

--- Serhii Nykon/NedoNet.API/UOF/IUnitOfWork.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Threading.Tasks;
-using NedoNet.API.Repositories;
-
-namespace NedoNet.API.UOF {
-    public interface IUnitOfWork {
-        IUsersRepository UserRepository { get; }
-    }
-}
