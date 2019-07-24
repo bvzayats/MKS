094210f400fd70d15dde6544c7cc1c93666cbad2
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-24 20:38:01 +0300

    MKS-46 Add UnitOfWork

---
+++ Serhii Nykon/NedoNet.API/UOF/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+using NedoNet.API.Repositories;
+
+namespace NedoNet.API.UOF {
+    public interface IUnitOfWork {
+        IUsersRepository UserRepository { get; }
+    }
+}
---
+++ Serhii Nykon/NedoNet.API/UOF/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using NedoNet.API.Repositories;
+
+namespace NedoNet.API.UOF {
+    public class UnitOfWork : IUnitOfWork {
+        private readonly string _connectionString = "Server=(localdb)\\mssqllocaldb;Database=NedoNet;Trusted_Connection=True;";
+        private readonly IMapper _mapper;
+        private IUsersRepository _usersRepository;
+
+        public UnitOfWork( IMapper mapper ) {
+            _mapper = mapper;
+        }
+
+        public IUsersRepository UserRepository {
+            get {
+                return _usersRepository = _usersRepository ?? new UsersRepository( _connectionString,  _mapper );
+            }
+        }
+    }
+}
