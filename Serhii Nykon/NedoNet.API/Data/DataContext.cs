4ee1f35c5e03461512a5debca6fda8b9adb6f5f8
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-24 20:01:41 +0300

    Add Users model

---
+++ Serhii Nykon/NedoNet.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NedoNet.API.Data.Models;
+
+namespace NedoNet.API.Data {
+    public class DataContext : DbContext {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
