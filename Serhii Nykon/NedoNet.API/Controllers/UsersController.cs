0c7144015c16f1aaabb973020538f5721b93a277
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-24 20:38:51 +0300

    MKS-46 Create UsersController

---
+++ Serhii Nykon/NedoNet.API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NedoNet.API.Repositories;
+using NedoNet.API.Services;
+
+namespace NedoNet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase {
+
+        private readonly IUsersService _usersService;
+
+        public UsersController( IUsersService usersService ) {
+            _usersService = usersService;
+        }
+
+        [HttpGet]
+        [Route( "{userId}" )]
+        public async Task<IActionResult> GetUser( Guid userId ) {
+
+            var user = await _usersService.GetUser( userId );
+            return Ok(user);
+        }
+    }
+}
