5490d325aabe6791fd7f1c9593e60871994b2188
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-24 22:45:31 +0300

    MKS-46 Create UUserViewEntity

---
+++ Serhii Nykon/NedoNet.API/Entities/UserViewEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace NedoNet.API.Entities {
+    public class UserViewEntity {
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
