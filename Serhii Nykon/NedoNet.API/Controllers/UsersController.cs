2b85859f176720912ab6f6a46e9eeb414c44144a
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-25 17:43:34 +0300

    MKS-46 Add endpoint for the user

---
+++ Serhii Nykon/NedoNet.API/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NedoNet.API.Data.Models;
+
+namespace NedoNet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase {
+        [HttpGet]
+        [Route("testUser")]
+        public IActionResult GetUser() {
+            return Ok( new User {
+                Id = Guid.NewGuid(),
+                Email = "testEmail",
+                FirstName = "testFirstName",
+                LastName = "testLastName",
+                PhoneNumber = "+380974925772"
+            } );
+        }
+    }
+}
