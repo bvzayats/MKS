3a54cb3cac8086fbd1333bbe6a7d68e1929c3838
SerhiiNykon <sergiy.nikon@jetsoftpro.com>
2019-07-25 23:07:37 +0300

    MKS-40 adjust codestyle for braces in SwaggerServiceExtennsion

--- Serhii Nykon/NedoNet.API/Extensions/SwaggerServiceExtensions.cs
+++ Serhii Nykon/NedoNet.API/Extensions/SwaggerServiceExtensions.cs
@@ -7,10 +7,8 @@ using Swashbuckle.AspNetCore.SwaggerUI;
 namespace NedoNet.API.Extensions {
     public static class SwaggerServiceExtensions {
         private static readonly string Version = "v1.0";
-        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
-        {
-            services.AddSwaggerGen(c =>
-            {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services) {
+            services.AddSwaggerGen(c => {
                 c.SwaggerDoc(Version, new Info { Title = $"BankingApp.API {Version}", Version = Version });
             });
 
@@ -20,8 +18,7 @@ namespace NedoNet.API.Extensions {
         public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
         {
             app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
+            app.UseSwaggerUI(c => {
                 c.SwaggerEndpoint($"/swagger/{Version}/swagger.json", $"Versioned API {Version}");
 
                 c.DocumentTitle = "Users API";
