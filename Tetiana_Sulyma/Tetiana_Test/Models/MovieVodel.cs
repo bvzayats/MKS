386ec0d256ea810027648ed7d784325cc88daca5
tann96 <sulymat@ukr.net>
2019-07-26 11:03:11 +0300

    MKS-35-Deleted unused libraries

--- Tetiana_Sulyma/Tetiana_Test/Models/MovieVodel.cs
+++ Tetiana_Sulyma/Tetiana_Test/Models/MovieVodel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Tetiana_Test.Models
+﻿namespace Tetiana_Test.Models
 {
     public class MovieVodel
     {
