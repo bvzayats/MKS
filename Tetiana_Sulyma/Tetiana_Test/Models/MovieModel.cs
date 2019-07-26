0b1cb43056020a7ddb9ee11166e5fab43299973e
tann96 <sulymat@ukr.net>
2019-07-26 16:26:04 +0300

    MKS-41 hotfix swagger error

---
+++ Tetiana_Sulyma/Tetiana_Test/Models/MovieModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Tetiana_Test.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Country { get; set; }
+        public float IMDb { get; set; }
+        public string Critical_acclaim { get; set; }
+    }
+}
