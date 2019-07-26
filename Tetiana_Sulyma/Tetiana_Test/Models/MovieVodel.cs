92c0e2e6bea0f53a6b728aa8e39d8251050d240f
tann96 <sulymat@ukr.net>
2019-07-26 10:50:41 +0300

    MKS-35 Create entity

---
+++ Tetiana_Sulyma/Tetiana_Test/Models/MovieVodel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tetiana_Test.Models
+{
+    public class MovieVodel
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Country { get; set; }
+        public float IMDb { get; set; }
+        public string Critical_acclaim { get; set; }
+    }
+}
