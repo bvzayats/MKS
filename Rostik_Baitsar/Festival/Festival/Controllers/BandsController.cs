d3b86d8a19ecd32b7eae35a8a4e9c0353ac372fb
Rostik <rostik1804@gmail.com>
2019-07-29 20:17:57 +0300

    MKS-45 Remove namespaces

--- Rostik_Baitsar/Festival/Festival/Controllers/BandsController.cs
+++ Rostik_Baitsar/Festival/Festival/Controllers/BandsController.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Festival.Models;
 using Festival.Servises;
 
--- Rostik_Baitsar/Festival/Festival/Servises/BandService.cs
+++ Rostik_Baitsar/Festival/Festival/Servises/BandService.cs
@@ -1,8 +1,6 @@
 ﻿using Festival.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Festival.Servises
