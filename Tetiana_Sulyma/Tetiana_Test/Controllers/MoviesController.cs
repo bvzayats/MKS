a5bf8fd92c235accf43d4a5bbe3912b166baaf74
tann96 <sulymat@ukr.net>
2019-07-29 20:29:21 +0300

    MKS-53 Replace to Services

---
+++ Tetiana_Sulyma/Tetiana_Test/Controllers/MoviesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Tetiana_Test.Models;
+using Tetiana_Test.Services;
+
+
+namespace Tetiana_Test.Controllers
+{
+    [Route("api/movies")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+          // GET api/movies
+        [HttpGet]
+        public ActionResult<IEnumerable<MovieModel>> Get()
+        {
+            List<MovieModel> movieModel = Tetiana_Test.Services.Services.Get();
+            return Ok(movieModel);
+                   
+         }
+
+        // GET api/movies/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/movies
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/movies/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/movies/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
